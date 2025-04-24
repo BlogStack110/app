da5b68461896cc1c1275360df6c1b12fe2ac54af
shivaraj110 <shivarajchandaragi9@gmail.com>
2025-04-24 19:47:06 +0530

    added tailwindcss

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,5 @@
 export default async function Home() {
-  await new Promise((r) => {
-    setTimeout(r, 3000);
-  });
-  return <div>Home Page</div>;
+  return (
+    <div className="flex justify-center items-center h-screen">Home Page</div>
+  );
 }
