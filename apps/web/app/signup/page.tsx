bb5da740a87bd0e07ac301b4a85e0312e6fdf2f6
shivaraj110 <shivarajchandaragi9@gmail.com>
2025-05-04 22:00:14 +0530

    defined callbacks

--- apps/web/app/signup/page.tsx
+++ apps/web/app/signup/page.tsx
@@ -1,53 +1,48 @@
 "use client";
 
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { redirect } from "next/navigation";
 import { authClient } from "../../lib/auth-client";
+import { CardsCreateAccount } from "@/components/ui/Signup";
 
 const Page = () => {
   const name: string = "shivaraj1110";
   const email: string = "shivarajchandaragi9@gmail.com";
   const password: string = "asdasdasssd";
   const [loading, setLoading] = useState<boolean>(false);
+  const onSigupEmail = useCallback(async () => {
+    const { data, error } = await authClient.signUp.email(
+      {
+        email, // user email address
+        password, // user password -> min 8 characters by default
+        name, // user display name
+        callbackURL: "/",
+      },
+      {
+        onRequest: (ctx) => {
+          setLoading(true);
+        },
+        onSuccess: (ctx) => {
+          redirect("/");
+        },
+        onError: (ctx) => {
+          // display the error message
+          alert(ctx.error.message);
+        },
+      },
+    );
+
+  }, [])
+
+  const onSigupGithub = useCallback(async () => {
+    const { data, error } = await authClient.signIn.social({
+      provider: "github",
+    });
+
+  }, [])
   return (
     <div className="flex h-screen justify-center items-center">
-      <button
-        onClick={async () => {
-          const { data, error } = await authClient.signUp.email(
-            {
-              email, // user email address
-              password, // user password -> min 8 characters by default
-              name, // user display name
-              callbackURL: "/",
-            },
-            {
-              onRequest: (ctx) => {
-                setLoading(true);
-              },
-              onSuccess: (ctx) => {
-                redirect("/");
-              },
-              onError: (ctx) => {
-                // display the error message
-                alert(ctx.error.message);
-              },
-            },
-          );
-        }}
-        className=" p-1 px-5 border size-fit border-gray-100 cursor-pointer shadow-lg"
-      >
-        Signup
-      </button>
-      <button
-        className="flex justify-center"
-        onClick={async () => {
-          const { data, error } = await authClient.signIn.social({
-            provider: "github",
-          });
-        }}
-      >
-        GitHub
-      </button>
+      <CardsCreateAccount />
     </div>
   );
 };
