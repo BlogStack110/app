46c4ca974f967342099eda61345785276ed85d72
shivaraj110 <shivarajchandaragi9@gmail.com>
2025-05-20 09:23:15 +0000

    created some functional server actions for bookmark mgmt

--- apps/web/app/actions/bookmarks.ts
+++ apps/web/app/actions/bookmarks.ts
@@ -1,49 +1,49 @@
-"use server"
-import { prisma } from "@/lib/db";
-import { randomUUID } from "node:crypto";
+'use server';
+import {prisma} from '@/lib/db';
+import {randomUUID} from 'node:crypto';
 
 export async function getBookmarks(userId: string) {
-  try {
-    const bookmarks = await prisma.bookmark.findMany({
-      where: {
-        userId: userId,
-      },
-    });
+	try {
+		const bookmarks = await prisma.bookmark.findMany({
+			where: {
+				userId: userId,
+			},
+		});
 
-    return bookmarks;
-  } catch (error) {
-    console.error("Error getting bookmarks:", error);
-    return [];
-  }
+		return bookmarks;
+	} catch (error) {
+		console.error('Error getting bookmarks:', error);
+		return [];
+	}
 }
 
 export async function addBookmark(userId: string, postId: string) {
-  try {
-    const bookmark = await prisma.bookmark.create({
-      data: {
-        id: randomUUID(),
-        userId: userId,
-        postId: postId,
-      },
-    });
+	try {
+		const bookmark = await prisma.bookmark.create({
+			data: {
+				id: randomUUID(),
+				userId: userId,
+				postId: postId,
+			},
+		});
 
-    return bookmark;
-  } catch (error) {
-    console.error("Error adding bookmark:", error);
-    return null;
-  }
+		return bookmark;
+	} catch (error) {
+		console.error('Error adding bookmark:', error);
+		return null;
+	}
 }
 
 export async function removeBookmark(id: string) {
-  try {
-    await prisma.bookmark.delete({
-      where: {
-        id
-      }
-    })
-    return true;
-  } catch (error) {
-    console.error("Error removing bookmark:", error);
-    return false;
-  }
+	try {
+		await prisma.bookmark.delete({
+			where: {
+				id,
+			},
+		});
+		return true;
+	} catch (error) {
+		console.error('Error removing bookmark:', error);
+		return false;
+	}
 }
