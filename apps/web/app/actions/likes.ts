d0d50b532ffd52d0c9731b4d607c3c0cf60938c6
shivaraj110 <shivarajchandaragi9@gmail.com>
2025-05-20 09:19:23 +0000

    created some functional server actions for likes mgmt

--- apps/web/app/actions/likes.ts
+++ apps/web/app/actions/likes.ts
@@ -1,50 +1,50 @@
-"use server"
-import { prisma } from "@/lib/db";
-import { randomUUID } from "node:crypto";
+'use server';
+import {prisma} from '@/lib/db';
+import {randomUUID} from 'node:crypto';
 
 export async function getLikes(userId: string) {
-  try {
-    const likes = await prisma.like.findMany({
-      where: {
-        userId: userId,
-      },
-    });
+	try {
+		const likes = await prisma.like.findMany({
+			where: {
+				userId: userId,
+			},
+		});
 
-    return likes;
-  } catch (error) {
-    console.error("Error getting likes:", error);
-    return [];
-  }
+		return likes;
+	} catch (error) {
+		console.error('Error getting likes:', error);
+		return [];
+	}
 }
 
 export async function addLike(userId: string, postId: string) {
-  try {
-    const like = await prisma.like.create({
-      data: {
-        id: randomUUID(),
-        userId: userId,
-        postId: postId,
-      },
-    });
+	try {
+		const like = await prisma.like.create({
+			data: {
+				id: randomUUID(),
+				userId: userId,
+				postId: postId,
+			},
+		});
 
-    return like;
-  } catch (error) {
-    console.error("Error adding like:", error);
-    return null;
-  }
+		return like;
+	} catch (error) {
+		console.error('Error adding like:', error);
+		return null;
+	}
 }
 
 export async function removeLike(id: string) {
-  try {
-    await prisma.like.deleteMany({
-      where: {
-        id
-      },
-    });
+	try {
+		await prisma.like.deleteMany({
+			where: {
+				id,
+			},
+		});
 
-    return true;
-  } catch (error) {
-    console.error("Error removing like:", error);
-    return false;
-  }
+		return true;
+	} catch (error) {
+		console.error('Error removing like:', error);
+		return false;
+	}
 }
