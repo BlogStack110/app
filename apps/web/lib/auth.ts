17fd1284efb9be00f45f0facc0089da408b5015e
shivaraj110 <shivarajchandaragi9@gmail.com>
2025-05-20 09:17:49 +0000

    modified lib files

--- apps/web/lib/auth.ts
+++ apps/web/lib/auth.ts
@@ -1,37 +1,36 @@
-import { betterAuth } from "better-auth";
-import { prismaAdapter } from "better-auth/adapters/prisma";
-import { prisma } from "./db";
-import { sendMail } from "./email";
+import {betterAuth} from 'better-auth';
+import {prismaAdapter} from 'better-auth/adapters/prisma';
+import {prisma} from './db';
+import {sendMail} from './email';
 export const auth = betterAuth({
-  database: prismaAdapter(prisma, {
-    provider: "postgresql",
-  }),
-  emailAndPassword: {
-    enabled: true,
-    requireEmailVerification: true,
-    autoSignIn: false
-  },
-  emailVerification: {
-    sendVerificationEmail: async ({ user, url }, request) => {
-      await sendMail({
-        from: "blogstack.site",
-        to: user.email,
-        subject: "Verify your email address",
-        html: `<h2><a href=${url}>Click the link to verify your email<a/></h2>`,
-      });
-    },
-    sendOnSignUp: true,
-    autoSignInAfterVerification: true,
-  },
-  socialProviders: {
-    github: {
-      clientId: process.env.GITHUB_CLIENT_ID as string,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
-    },
-    google: {
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    },
-  },
-}
-)
+	database: prismaAdapter(prisma, {
+		provider: 'postgresql',
+	}),
+	emailAndPassword: {
+		enabled: true,
+		requireEmailVerification: true,
+		autoSignIn: false,
+	},
+	emailVerification: {
+		sendVerificationEmail: async ({user, url}, request) => {
+			await sendMail({
+				from: 'blogstack.site',
+				to: user.email,
+				subject: 'Verify your email address',
+				html: `<h2><a href=${url}>Click the link to verify your email<a/></h2>`,
+			});
+		},
+		sendOnSignUp: true,
+		autoSignInAfterVerification: true,
+	},
+	socialProviders: {
+		github: {
+			clientId: process.env.GITHUB_CLIENT_ID as string,
+			clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+		},
+		google: {
+			clientId: process.env.GOOGLE_CLIENT_ID as string,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+		},
+	},
+});
