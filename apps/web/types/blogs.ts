20aa819159ec2f81d4b14cc9518d20231c5d7b68
shivaraj110 <shivarajchandaragi9@gmail.com>
2025-06-07 11:17:13 +0530

    added /blog back

--- apps/web/types/blogs.ts
+++ apps/web/types/blogs.ts
@@ -38,6 +38,23 @@ export interface RelatedPosts {
 	};
 }
 
+export interface DashboardBlogData {
+	authorName: string;
+	title: string;
+	authorId: string;
+	content: string;
+	publishDate: string;
+	comments: number;
+	views: number;
+	tags: string[];
+	likes: number;
+	id: string;
+	likeCount: number;
+	imgUrl: string;
+	authorImgUrl: string;
+	bookmarked: boolean;
+	deleteable?: boolean;
+}
 
 export interface featuredBlog {
 	author: {
