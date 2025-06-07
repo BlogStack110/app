602627f3b4992d426778496f8601d9c3cc13416f
shivaraj110 <shivarajchandaragi9@gmail.com>
2025-06-07 12:03:30 +0530

    added layout

--- apps/web/app/blog/[id]/page.tsx
+++ apps/web/app/blog/[id]/page.tsx
@@ -1,18 +1,24 @@
 import { getBlogDetails } from '@/app/api/blogs';
 import Blog from '@/components/Blog';
+import PublicFooter from '@/components/PublicFooter';
+import PublicNavbar from '@/components/PublicNavbar';
 
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
-  const { id } = await params;
-  const blogDetails = await getBlogDetails(id);
-  const blog = blogDetails?.blog;
-  const relatedPosts = blogDetails?.relatedPosts;
-  // const handleCommentSubmit = () => {};
-  return (
-    <Blog
-      blog={blog}
-      relatedPosts={relatedPosts}
-    />
-  );
+	const { id } = await params;
+	const blogDetails = await getBlogDetails(id);
+	const blog = blogDetails?.blog;
+	const relatedPosts = blogDetails?.relatedPosts;
+	// const handleCommentSubmit = () => {};
+	return (
+		<>
+			<PublicNavbar />
+			<Blog
+				blog={blog}
+				relatedPosts={relatedPosts}
+			/>
+			<PublicFooter />
+		</>
+	);
 }
 
 // function stripHtml(html: string): string {
