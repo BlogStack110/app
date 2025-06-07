8b71230ec78c8e3b27f6a70c3b003d8d4d898396
shivaraj110 <shivarajchandaragi9@gmail.com>
2025-06-07 15:37:28 +0530

    fixed fething of server side session

--- apps/web/app/blog/[id]/page.tsx
+++ apps/web/app/blog/[id]/page.tsx
@@ -8,7 +8,6 @@ export default async function Page({ params }: { params: Promise<{ id: string }>
 	const blogDetails = await getBlogDetails(id);
 	const blog = blogDetails?.blog;
 	const relatedPosts = blogDetails?.relatedPosts;
-	// const handleCommentSubmit = () => {};
 	return (
 		<>
 			<PublicNavbar />
@@ -21,13 +20,4 @@ export default async function Page({ params }: { params: Promise<{ id: string }>
 	);
 }
 
-// function stripHtml(html: string): string {
-//   // Check if we're in the browser environment
-//   if (typeof document !== "undefined") {
-//     const doc = new DOMParser().parseFromString(html, "text/html");
-//     return doc.body.textContent || "";
-//   }
-//
-//   // Server-side fallback - simple regex to strip HTML tags
-//   return html.replace(/<[^>]*>?/gm, "");
-//
+
