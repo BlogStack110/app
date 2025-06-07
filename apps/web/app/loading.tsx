1e0a9849f72c59b4c9a8d4933be916dd57f72e9b
shivaraj110 <shivarajchandaragi9@gmail.com>
2025-06-07 12:04:40 +0530

    added loading spinner

--- apps/web/app/loading.tsx
+++ apps/web/app/loading.tsx
@@ -1,5 +1,24 @@
 import React from 'react';
-const Loader = () => {
-	return <div>Loading...</div>;
+
+const Loader: React.FC = () => {
+	return (
+		<div className="flex fixed inset-0 items-center justify-center ">
+			<div className="flex space-x-1">
+				<div
+					className="w-2 h-2 bg-blue-500 rounded-full animate-pulse"
+					style={{ animationDelay: '0ms' }}
+				/>
+				<div
+					className="w-2 h-2 bg-blue-500 rounded-full animate-pulse"
+					style={{ animationDelay: '150ms' }}
+				/>
+				<div
+					className="w-2 h-2 bg-blue-500 rounded-full animate-pulse"
+					style={{ animationDelay: '300ms' }}
+				/>
+			</div>
+		</div>
+	);
 };
+
 export default Loader;
