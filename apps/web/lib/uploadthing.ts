636cade0bd052a520e50d375531b1c4841bf8f3a
shivaraj110 <shivarajchandaragi9@gmail.com>
2025-06-28 10:30:51 +0530

    updated config

--- apps/web/lib/uploadthing.ts
+++ apps/web/lib/uploadthing.ts
@@ -1,10 +1,11 @@
 
+import { UploadRouter } from "@/app/api/uploadthing/core";
 import {
 	generateUploadButton,
 	generateUploadDropzone,
 } from "@uploadthing/react";
 
-export const UploadButton = generateUploadButton();
-export const UploadDropzone = generateUploadDropzone();
+export const UploadButton = generateUploadButton<UploadRouter>();
+export const UploadDropzone = generateUploadDropzone<UploadRouter>();
 
 
