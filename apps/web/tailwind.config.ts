869d1c5ae7eab41dc35eaa2ea0024dba88f010ef
shivaraj110 <shivarajchandaragi9@gmail.com>
2025-06-28 10:17:54 +0530

    added tailwind config

---
+++ apps/web/tailwind.config.ts
@@ -0,0 +1,16 @@
+import { withUt } from "uploadthing/tw";
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default withUt(config);
