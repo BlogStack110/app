d8472b53ccf662f81c6ed5a353b5335e273563bb
shivaraj110 <shivarajchandaragi9@gmail.com>
2025-07-02 00:03:36 +0530

    removed hooks folder

--- apps/web/app/hooks/useUrl.ts
+++
@@ -1,8 +0,0 @@
-import { useSearchParams } from "next/navigation"
-
-const usePageNumber = () => {
-  const searchParams = useSearchParams()
-  const pageNumber: number = Number(searchParams.get("page"))
-  return pageNumber ? pageNumber : 1
-}
-export default usePageNumber
