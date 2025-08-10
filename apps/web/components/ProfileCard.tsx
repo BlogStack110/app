421674af653831fdcb25dbb01e50663e9b0e30f9
shivaraj110 <shivarajchandaragi9@gmail.com>
2025-08-10 22:57:06 +0530

    feat: read about user btn & badge info

--- apps/web/components/ProfileCard.tsx
+++ apps/web/components/ProfileCard.tsx
@@ -1,18 +1,20 @@
-import { FacebookIcon, GithubIcon, LinkedinIcon, MessageSquare, MoreHorizontal, TwitchIcon, TwitterIcon, Verified, Youtube } from "lucide-react"
+"use client";
+import { motion } from "motion/react";
+import { BookOpenTextIcon, FacebookIcon, FlaskConical, GithubIcon, LinkedinIcon, MoreHorizontal, TwitchIcon, TwitterIcon, Verified, Youtube } from "lucide-react"
 import Image from "next/image"
-import {
-	Drawer,
-	DrawerClose,
-	DrawerContent,
-	DrawerDescription,
-	DrawerFooter,
-	DrawerHeader,
-	DrawerTitle,
-	DrawerTrigger,
-} from "@/components/ui/drawer"
-import { Button } from "./ui/button";
 import { Profile } from "@/types/profile";
-
+import { useState } from "react";
+import {
+	Dialog,
+	DialogClose,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "@/components/ui/dialog"
+import { formatDate } from "@/lib/dateFunction";
 const SocialIcon = ({ link }: { link: string }) => {
 
 	const name = link.includes("github") ? "github" : link.includes("twitter") ?
@@ -38,11 +40,23 @@ const SocialIcon = ({ link }: { link: string }) => {
 
 
 export const ProfileCard = ({ props }: { props: Profile }) => {
+
+	const [readMore, setReadMore] = useState(false);
+
 	return (
-		<div className="flex flex-col mx-auto h-fit min-w-7xl max-w-[1280px] -translate-y-[60%] w-full">
-			<div className="flex flex-col bg-gradient-to-r from-neutral-800/90 to-neutral-900/95 rounded-lg shadow-md p-4 h-56 backdrop-blur-lg">
+		<motion.div
+			transition={{
+				duration: 0.35,
+				delay: 0,
+				ease: "easeInOut",
+			}}
+			animate={{ y: 0 }}
+			initial={{ y: -100 }}
+			className="flex flex-col justify-center items-center h-full min-w-7xl max-w-[1280px] z-50 -translate-y-32 w-full" >
+			<div
+				className="flex flex-col bg-gradient-to-r from-neutral-800/90 to-neutral-900/95 rounded-lg w-7xl  shadow-xl p-4 pb-5 h-fit backdrop-blur-lg">
 				{/* Top section */}
-				<div className="flex justify-between">
+				<div className="flex justify-between transition-all duration-500 ease-in-out">
 
 					{/* Left top section*/}
 					<div className="flex items-center gap-4">
@@ -78,8 +92,6 @@ export const ProfileCard = ({ props }: { props: Profile }) => {
 									)
 									)
 								}
-
-
 							</div>
 						</div>
 
@@ -100,50 +112,52 @@ export const ProfileCard = ({ props }: { props: Profile }) => {
 				{/* Bottom section */}
 				<div className="grid grid-cols-2 justify-between">
 					{/* Left bottom section */}
-					<div className="flex flex-col col-span-1 space-y-1 ">
-						<h4 className="text-xl text-neutral-300 font-semibold">About</h4>
-						<div className="text-md text-neutral-500">
-							{props.about.length > 125 ? props.about.slice(0, 125) + "..." : props.about}
-
-							<Drawer>
-								<DrawerTrigger asChild>
-
-									<span className="font-semibold text-blue-400 cursor-pointer">Read More</span>
-								</DrawerTrigger>
-								<DrawerContent className="border-none rounded-tl-3xl rounded-tr-3xl">
-									<div className="mx-auto w-full flex flex-col justify-between space-y-3 max-w-[1280px] ">
-										<DrawerHeader>
-											<DrawerTitle className="text-neutral-400">{"About " + props.fullName}</DrawerTitle>
-										</DrawerHeader>
-										<div className=" pb-0">
-											<div className="flex text-lg font-semibold text-neutral-400 self-center">
-												{props.about}
-											</div>
-										</div>
-										<DrawerFooter>
-											<DrawerClose asChild>
-												<Button className="w-[200px] self-center" variant="outline">Back</Button>
-											</DrawerClose>
-										</DrawerFooter>
-									</div>
-								</DrawerContent>
-							</Drawer>
-						</div>
-					</div>
+					<div className="flex flex-col bg-gradient-to-r from-neutral-800/10 to-neutral-900/15 rounded-lg backdrop-blur-2xl col-span-1 space-y-1">
+						<section className="grid place-items-center ">
+							<label>
+								<input className="peer/showLabel absolute scale-0" type="checkbox" />
+								<span className="block max-h-14 max-w-7xl overflow-hidden rounded-lg px-4 py-0 text-neutral-600 inset-shadow-neutral-950/10 transition-all duration-300 peer-checked/showLabel:max-h-52">
+									<h3 className="flex h-14 cursor-pointer items-center font-bold hover:text-blue-600/30 transition-colors duration-500 delay-75">Read About  {"  " + props.fullName}</h3>
+									<p className="mb-2 text-neutral-500 font-semibold">{props.about.slice(0, 300)}</p>
+								</span>
+							</label>
+						</section>					</div>
 					{/* Right bottom section */}
 					<div className="flex col-span-1 justify-end items-end space-x-2">
-						<div className="text-md font-semibold bg-neutral-900 text-neutral-500 size-fit p-1 px-2 rounded-md inset-shadow-neutral-950">
-							Developer
-						</div>
-						<div className="text-md font-semibold bg-neutral-900 text-neutral-500 size-fit p-1 px-2 rounded-md inset-shadow-neutral-950">
-							Contributor
-						</div>
-						<div className="text-md font-semibold bg-neutral-900 text-neutral-500 size-fit p-1 px-2 rounded-md inset-shadow-neutral-950">
-							Admin
-						</div>
+
+						{
+							props.badges?.map((badge, index) => (
+								<Dialog >
+									<form>
+										<DialogTrigger asChild>
+											<div key={index} className="text-md font-semibold bg-neutral-900 text-neutral-600 size-fit hover:text-blue-500/50 transition-colors duration-500 delay-100 cursor-pointer p-1 px-2 rounded-md inset-shadow-neutral-950">
+												{badge}
+											</div>
+										</DialogTrigger>
+										<DialogContent className="sm:max-w-[425px] bg-neutral-900 flex flex-col text-neutral-600 items-center border-none">
+											<DialogHeader className="flex flex-col items-center">
+												<DialogTitle className="px-2 py-1  bg-yellow-200/10 border border-yellow-500/30 rounded-lg text-neutral-400"> {badge}</DialogTitle>
+												<DialogDescription>
+													issued {formatDate(new Date().toUTCString())}
+												</DialogDescription>
+											</DialogHeader>
+											<FlaskConical strokeWidth={0.25} className=" size-28 border-none text-yellow-200 animate-pulse animate-collapsible-up fill-yellow-200/10" />
+											<div className="text-center">
+												This writer has been recognized for consistently producing high-quality, error-free content that enhances our community's reading experience
+											</div>
+											<DialogFooter>
+												<DialogClose asChild>
+												</DialogClose>
+											</DialogFooter>
+										</DialogContent>
+									</form>
+								</Dialog>
+
+							))
+						}
 					</div>
 				</div>
 			</div>
-		</div >
+		</motion.div >
 	)
 }
