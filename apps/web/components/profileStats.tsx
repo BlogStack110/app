bd4878526a79343f24eb6bad92708dc1295ab884
shivaraj110 <shivarajchandaragi9@gmail.com>
2025-08-31 22:04:12 +0530

    init proflestats comp

---
+++ apps/web/components/profileStats.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { motion } from "motion/react";
+import { useState } from "react";
+export const ProfileStats = (props: {}) => {
+  const navItems: { title: string }[] = [
+    { title: "Overview" },
+    { title: "Followers" },
+    { title: "Following" },
+    { title: "Achievements" },
+  ];
+
+  const [currentItem, setCurrentItem] = useState<string | undefined>(
+    navItems[0]?.title,
+  );
+
+  return (
+    <motion.div
+      transition={{
+        duration: 0.35,
+        delay: 0,
+        ease: "easeInOut",
+      }}
+      animate={{ y: 0 }}
+      initial={{ y: -100 }}
+      className="flex flex-col justify-center items-center h-full min-w-7xl max-w-[1280px] z-50 w-full"
+    >
+      <div className="flex bg-gradient-to-r bg-neutral-900/85 justify-start space-x-28 p-5 rounded-lg w-7xl shadow-xl backdrop-blur-xl">
+        {navItems.map((item, index) => (
+          <div
+            onClick={() => {
+              setCurrentItem(item.title);
+            }}
+            key={index}
+            className={`text-lg ${currentItem === item.title ? "text-neutral-400 decoration-blue-500 underline decoration-4 underline-offset-8" : "text-neutral-500"} font-medium  cursor-pointer hover:text-neutral-400 transition-all duration-300 delay-75`}
+          >
+            {item.title}
+          </div>
+        ))}
+      </div>
+    </motion.div>
+  );
+};
